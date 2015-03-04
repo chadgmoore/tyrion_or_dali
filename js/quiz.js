f2781c6e71327f6b45fe05e542575f7ffc58dc8c
chadgmoore <smap2d@gmail.com>
2015-03-04 13:11:16 -0500

    loading random quote, tyrion button are functional

--- js/quiz.js
+++ js/quiz.js
@@ -1,3 +1,6 @@
+// make sure wuote isn't already used
+// track question numbers and update icons accordingly
+
 function loadInstructions(){
   $('.instructions').delay(800).animate({opacity: 1, duration: "slow", easing: "easein"});
 };
@@ -24,13 +27,35 @@ function getRandonQuote(){
   ];
 
   var foundQuote = Math.floor(Math.random()*quotes.length);
-  var author = quotes[foundQuote].split('--')[0];
+  var correctAuthor = quotes[foundQuote].split('--')[0];
   var quoteToDisplay = quotes[foundQuote].split('--')[1];
-  console.log(author, quoteToDisplay);
+  console.log(correctAuthor, quoteToDisplay);
   $('.questions').children('p').empty();
   $('.questions').children('p').append('<p><i class="fa fa-quote-left fa-1x"></i>"' + quoteToDisplay + '<i class="fa fa-quote-right fa-1x"></i></p>');
   
-  return (author, quoteToDisplay);
+  return (correctAuthor, quoteToDisplay);
+};
+
+function checkAnswer(guessedAuthor, correctAuthor){
+  if (guessedAuthor == correctAuthor){
+    console.log('CORRECT');
+    var answerStatus = 'correct';
+  }
+  else{
+    console.log('INCORRECT')
+    var answerStatus = 'incorrect'
+  }
+  setStatus(answerStatus);
+};
+
+function setStatus(answerStatus){
+  //count the questions?
+  if (answerStatus == 'correct'){
+    $('#q1').css('color', '#00FF00')
+  }
+  else {
+    $('#q1').css('color', '#FF0000')
+  }
 };
 
 $(document).ready(function() {
@@ -41,5 +66,15 @@ $('.instructions').on('click', function(event){
   $(this).animate({opacity: 0, duration: "slow", easing: "easein"});
   newGame();
   });
+
+
+$('.buttonTyrion').on('click', function(correctAuthor){
+  var guessedAuthor = 'tyrion';
+  // correctAuthor = correctAuthor.value();
+  console.log('from tyrion button guessed - <' + guessedAuthor + '> - correct <' + correctAuthor + '>.')
+  checkAnswer(guessedAuthor, correctAuthor);
+});
+
+
 
 });
